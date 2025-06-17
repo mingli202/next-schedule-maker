ee96ab072980675fb5855077cd42af7a7b11d23c
mingli202 <vincentmingli@gmail.com>
2025-06-16 21:27:09 -0400

    moved models

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -1,15 +1,5 @@
 import { z } from "zod";
 
-abstract class ModelBase {
-  public schema: z.ZodType;
-
-  constructor(schema: z.ZodType) {
-    this.schema = schema;
-  }
-}
-
-export class SomeTime extends ModelBase {}
-
 export const Time = z.record(
   z.enum(["M", "T", "W", "R", "F"]),
   z.array(z.string()),
@@ -36,27 +26,27 @@ export const ViewData = z.array(
   z.record(z.number().min(1).max(5), z.array(z.number().min(1).max(20))),
 );
 
-export const Section = {
-  schema: z.object({
-    program: z.string().regex(/[A-Z]+/),
-    count: z.number(),
-    section: z.string().regex(/^[0-9]{5}$/),
-    course: z.string(),
-    code: z.string().regex(/\w{3}-\w{3}-\w{1,2}/),
-    lecture: LecLab.optional(),
-    lab: LecLab.optional(),
-    more: z.string(),
-    viewData: ViewData,
-  }),
-};
+export const Section = z.object({
+  program: z.string().regex(/[A-Z]+/),
+  count: z.number(),
+  section: z.string().regex(/^[0-9]{5}$/),
+  course: z.string(),
+  code: z.string().regex(/\w{3}-\w{3}-\w{1,2}/),
+  lecture: LecLab.optional(),
+  lab: LecLab.optional(),
+  more: z.string(),
+  viewData: ViewData,
+});
 
 export const Professors = z.array(z.string());
 export const Colors = z.array(z.string());
+export const AllClasses = z.record(z.number(), Section);
 
 export type Time = z.infer<typeof Time>;
 export type Rating = z.infer<typeof Rating>;
 export type LecLab = z.infer<typeof LecLab>;
 export type ViewData = z.infer<typeof ViewData>;
-export type Section = z.infer<typeof Section.schema>;
+export type Section = z.infer<typeof Section>;
 export type Professors = z.infer<typeof Professors>;
 export type Colors = z.infer<typeof Colors>;
+export type AllClasses = z.infer<typeof AllClasses>;
