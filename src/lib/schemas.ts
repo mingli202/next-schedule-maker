9c141ab12c757d4151d9fa8ae280daa09650cbbb
mingli202 <vincentmingli@gmail.com>
2025-06-17 21:02:04 -0400

    renamed schemas

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,11 +1,11 @@
 import { z } from "zod/v4";
 
-export const TimeSchema = z.record(
+export const Time = z.record(
   z.enum(["M", "T", "W", "R", "F"]),
   z.array(z.string()),
 );
 
-export const RatingSchema = z.object({
+export const Rating = z.object({
   score: z.number().min(0.0).max(100.0),
   avg: z.number().min(0.0).max(5.0),
   nRating: z.number().min(0.0),
@@ -15,38 +15,38 @@ export const RatingSchema = z.object({
   prof: z.string(),
 });
 
-export const LecLabSchema = z.object({
+export const LecLab = z.object({
   title: z.string(),
   prof: z.string(),
-  time: TimeSchema,
-  rating: RatingSchema.optional(),
+  time: Time,
+  rating: Rating.optional(),
 });
 
-export const ViewDataSchema = z.array(
+export const ViewData = z.array(
   z.record(z.number().min(1).max(5), z.array(z.number().min(1).max(20))),
 );
 
-export const SectionSchema = z.object({
+export const Section = z.object({
   program: z.string().regex(/[A-Z]+/),
   count: z.number(),
   section: z.string().regex(/^[0-9]{5}$/),
   course: z.string(),
   code: z.string().regex(/\w{3}-\w{3}-\w{1,2}/),
-  lecture: LecLabSchema.optional(),
-  lab: LecLabSchema.optional(),
+  lecture: LecLab.optional(),
+  lab: LecLab.optional(),
   more: z.string(),
-  viewData: ViewDataSchema,
+  viewData: ViewData,
 });
 
-export const ProfessorsSchema = z.array(z.string());
-export const ColorsSchema = z.array(z.string());
-export const AllClassesSchema = z.record(z.number(), SectionSchema);
+export const Professors = z.array(z.string());
+export const Colors = z.array(z.string());
+export const AllClasses = z.record(z.number(), Section);
 
-export type TimeType = z.infer<typeof TimeSchema>;
-export type RatingType = z.infer<typeof RatingSchema>;
-export type LecLabType = z.infer<typeof LecLabSchema>;
-export type ViewDataType = z.infer<typeof ViewDataSchema>;
-export type SectionType = z.infer<typeof SectionSchema>;
-export type ProfessorsType = z.infer<typeof ProfessorsSchema>;
-export type ColorsType = z.infer<typeof ColorsSchema>;
-export type AllClassesType = z.infer<typeof AllClassesSchema>;
+export type Time = z.infer<typeof Time>;
+export type Rating = z.infer<typeof Rating>;
+export type LecLab = z.infer<typeof LecLab>;
+export type ViewData = z.infer<typeof ViewData>;
+export type Section = z.infer<typeof Section>;
+export type Professors = z.infer<typeof Professors>;
+export type Colors = z.infer<typeof Colors>;
+export type AllClasses = z.infer<typeof AllClasses>;
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,16 +1,19 @@
-import { Class } from "@/types";
+import { Section } from "./schemas";
 
-export function getSectionTimes(section: Class): string[][] {
+export function getSectionTimes(
+  section: Section,
+  leclab?: "lecture" | "lab",
+): string[][] {
   let times: string[][] = [];
 
-  if (section.lecture) {
+  if (section.lecture && (!leclab || leclab === "lecture")) {
     const time = Object.entries(section.lecture.time).flatMap(([d, ts]) =>
       ts.map((t) => [d, t]),
     );
     times = [...times, ...time];
   }
 
-  if (section.lab) {
+  if (section.lab && (!leclab || leclab === "lab")) {
     const time = Object.entries(section.lab.time).flatMap(([d, ts]) =>
       ts.map((t) => [d, t]),
     );
