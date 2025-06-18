ca8d4d58556f1a9d04f309c1493df33209f578c1
mingli202 <vincentmingli@gmail.com>
2025-06-17 22:12:27 -0400

    feat: RecordValues util

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -2,4 +2,7 @@ import cn from "./cn";
 import { getRemoteJson, DataFilename } from "./getRemoteJson";
 import { getSectionTimes } from "./getSectionTimes";
 
+export type RecordValues<T extends Record<string | number | symbol, unknown>> =
+  T[keyof T];
+
 export { cn, getRemoteJson, DataFilename, getSectionTimes };
