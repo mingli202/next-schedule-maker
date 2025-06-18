3b4949971d48d6b4b06fa40c8203dea21106bd2a
mingli202 <vincentmingli@gmail.com>
2025-06-17 22:12:50 -0400

    feat: database types

--- src/lib/schemas/database.ts
+++ src/lib/schemas/database.ts
@@ -1,17 +1,33 @@
 import { z } from "zod/v4";
-import { ViewData } from "./pdf";
+import { SemesterNames } from "../util/semesterNames";
 
-export const ScheduleData = z.object({
-  data: z.array(ViewData),
+export const Saved = z.object({
+  bgColor: z.string(),
+  textColor: z.string(),
+  id: z.number(),
+});
+
+export const SavedScheduleData = z.object({
+  data: z.array(Saved),
   name: z.string(),
-  semester: z.literal("fall2025"),
+  semester: z.literal(SemesterNames.current),
 });
 
 export const Database = z.object({
   users: z.record(
     z.string(),
     z.object({
-      schedules: ScheduleData,
+      schedules: SavedScheduleData,
+    }),
+  ),
+  reports: z.record(
+    z.number(),
+    z.object({
+      count: z.number(),
+      reason: z.string(),
     }),
   ),
 });
+
+export type SavedScheduleData = z.infer<typeof SavedScheduleData>;
+export type Database = z.infer<typeof Database>;
