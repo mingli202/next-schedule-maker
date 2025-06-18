3a51bf69733daaf9ea3663417fdfeb6d05e48260
mingli202 <vincentmingli@gmail.com>
2025-06-17 21:44:33 -0400

    moved to env

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -3,14 +3,13 @@ import { GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const config = {
-  apiKey: "AIzaSyAG8LJPNXNgDKnfopB-c1CVX-Uew4uRyqU",
-  authDomain: "schedule-maker-8675b.firebaseapp.com",
-  databaseURL: "https://schedule-maker-8675b-default-rtdb.firebaseio.com",
-  projectId: "schedule-maker-8675b",
-  storageBucket: "schedule-maker-8675b.appspot.com",
-  messagingSenderId: "198447247334",
-  appId: "1:198447247334:web:314fe11a846b7f973cf027",
-  measurementId: "G-VHKK3NNF8T",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 const app = initializeApp(config);
