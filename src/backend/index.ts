a50db66efa405a5f34814026822244d20602b307
mingli202 <vincentmingli@gmail.com>
2025-06-18 23:09:11 -0400

    attempted some thing

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,6 +1,7 @@
+import { Database, User } from "@/lib/schemas/database";
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+import { get, getDatabase, ref } from "firebase/database";
 
 const config = {
   apiKey: process.env.apiKey,
@@ -15,5 +16,47 @@ const config = {
 const app = initializeApp(config);
 const db = getDatabase(app);
 const provider = new GoogleAuthProvider();
+
+class Base {}
+
+class Save extends Base {}
+
+class SavedSchedule extends Base {
+  private _data: Save[];
+  private _textColor: string;
+  private _id: number;
+}
+
+class Db extends Base {
+  private _schema = Database;
+  private _db;
+
+  constructor() {
+    super();
+    this._db = getDatabase(app);
+  }
+
+  public async users(): Promise<Record<string, User>> {
+    const snapshot = await get(ref(this._db, "users"));
+
+    if (!snapshot.exists()) {
+      return {};
+    }
+
+    const users = this._schema.shape.users.parse(snapshot.val());
+
+    return users;
+  }
+
+  public async users(uid: string): Promise<User | null> {
+    const snapshot = await get(ref(this._db, `users/${uid}`));
+
+    if (!snapshot.exists()) {
+      return null;
+    }
+
+    return User.parse(snapshot.val());
+  }
+}
 
 export { app, db, provider };
