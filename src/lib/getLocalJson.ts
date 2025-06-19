9f275b7cea0235a6689a29a577583389c519c610
mingli202 <vincentmingli@gmail.com>
2025-06-18 23:18:30 -0400

    enable caching cuz im being cooked fuck

--- src/lib/getLocalJson.ts
+++ src/lib/getLocalJson.ts
@@ -8,7 +8,7 @@ export type DataFilename = (typeof DataFilename)[keyof typeof DataFilename];
 export const getLocalJsonData = async <T>(name: string): Promise<T> => {
   const url = `https://raw.githubusercontent.com/mingli202/next-schedule-maker/fall2025/public/json/${name}.json`;
 
-  const res = await fetch(url, { cache: "no-cache" });
+  const res = await fetch(url);
   if (!res.ok) throw new Error("fetch failed: " + res.statusText);
   const toReturn: T = await res.json();
   return toReturn;
