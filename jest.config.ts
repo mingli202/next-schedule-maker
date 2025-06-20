8dd27be30ebd153770edff1aa4ef02140ad9569e
mingli202 <vincentmingli@gmail.com>
2025-06-19 20:31:02 -0400

    test(config): updated config to work with next

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,12 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -198,4 +203,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+export default createJestConfig(config);
