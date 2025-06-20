1c1519f5d92e23332b223025985acba9772dc7e2
mingli202 <vincentmingli@gmail.com>
2025-06-20 19:10:54 -0400

    updated getlocaljson

--- src/lib/getLocalJson.ts
+++ src/lib/getLocalJson.ts
@@ -6,9 +6,10 @@ export const DataFilename = {
 export type DataFilename = (typeof DataFilename)[keyof typeof DataFilename];
 
 export const getLocalJsonData = async <T>(name: string): Promise<T> => {
-  const url = `https://raw.githubusercontent.com/mingli202/next-schedule-maker/fall2025/public/json/${name}.json`;
+  const commit = "7461c29a3ea4f4648a7424d4246fbf417fe4877d";
+  const url = `https://raw.githubusercontent.com/mingli202/next-schedule-maker/${commit}/public/json/${name}.json`;
 
-  const res = await fetch(url, { cache: "no-cache" });
+  const res = await fetch(url);
   if (!res.ok) throw new Error("fetch failed: " + res.statusText);
   const toReturn: T = await res.json();
   return toReturn;
