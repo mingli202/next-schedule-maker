07fd8f3d665d168dbe89ad16a0729d75cd86cbf3
mingli202 <vincentmingli@gmail.com>
2025-06-22 18:49:07 -0400

    made Ref class, may not use it

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -1,10 +1,35 @@
 import cn from "./cn";
 import { getRemoteJson, DataFilename } from "./getRemoteJson";
 import { getSectionTimes } from "./getSectionTimes";
+import { Clone } from "./interfaces";
 
 export type RecordValues<T extends Record<string | number | symbol, unknown>> =
   T[keyof T];
 
 export type Self<T> = { new (): T };
 
-export { cn, getRemoteJson, DataFilename, getSectionTimes };
+class Ref<T> implements Clone {
+  #val: T;
+
+  constructor(val: T) {
+    this.#val = val;
+  }
+
+  public set(newVal: T) {
+    this.#val = newVal;
+  }
+
+  public get(): T {
+    return this.#val;
+  }
+
+  public getCloned(): T {
+    return this.clone().get();
+  }
+
+  clone(): this {
+    return structuredClone(this);
+  }
+}
+
+export { cn, getRemoteJson, DataFilename, getSectionTimes, Ref };
