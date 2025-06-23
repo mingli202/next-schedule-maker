71f5b00a9c0889443621e1b33082170983e64656
mingli202 <vincentmingli@gmail.com>
2025-06-22 23:41:18 -0400

    fix: getRemoteJson

--- src/lib/util/getRemoteJson.ts
+++ src/lib/util/getRemoteJson.ts
@@ -1,17 +1,31 @@
+import { z } from "zod/v4";
 import { AllClasses, Colors, Professors } from "../schemas/pdf";
 import { SemesterNames } from "./semesterNames";
+import { Err, Ok, Result } from "./result";
+import { RecordValues } from ".";
 
 export const DataFilename = {
   allClasses: { filename: "allClasses", schema: AllClasses },
   colors: { filename: "colors", schema: Colors },
   professors: { filename: "professors", schema: Professors },
 };
-export type DataFilename = (typeof DataFilename)[keyof typeof DataFilename];
+export type DataFilename = RecordValues<typeof DataFilename>;
 
-export async function getRemoteJson(name: DataFilename) {
+export async function getRemoteJson<
+  T extends z.output<AllClasses | Colors | Professors>,
+>(name: DataFilename): Promise<Result<T, string>> {
   const url = `https://raw.githubusercontent.com/mingli202/next-schedule-maker/${SemesterNames.Current}/public/json/${name.filename}.json`;
 
   const res = await fetch(url, { cache: "no-cache" });
-  if (!res.ok) throw new Error("fetch failed: " + res.statusText);
-  return name.schema.safeParse(await res.json());
+
+  if (!res.ok)
+    return new Err(`failted to fetch ${name} from github: ${res.statusText}`);
+
+  const safeParseRes = name.schema.safeParse(await res.json());
+
+  if (safeParseRes.success) {
+    return new Ok(safeParseRes.data as T);
+  }
+
+  return new Err(safeParseRes.error.message);
 }
