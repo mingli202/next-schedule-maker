0ab9a3b00b7a5de41185555549efde107cc9f493
mingli202 <vincentmingli@gmail.com>
2025-12-26 19:31:00 -0500

    feat: button actual mask

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { ButtonHTMLAttributes, useRef } from "react";
-import { HTMLMotionProps, motion, useAnimate } from "framer-motion";
+import { ButtonHTMLAttributes, useRef, useState } from "react";
+import {
+  AnimatePresence,
+  HTMLMotionProps,
+  motion,
+  useAnimate,
+} from "framer-motion";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib";
 
@@ -23,8 +28,10 @@ function Button({
   targetPath,
   ...props
 }: Props) {
+  const yellow = "#facc15";
   const hoverVariants = {
     // basic animation for non important text
+
     basic: {
       scale: disableScaleEffect ? 1 : 1.01,
       opacity: 1,
@@ -32,9 +39,8 @@ function Button({
     // for important buttons
     special: {
       scale: disableScaleEffect ? 1 : 1.01,
-      color: "#facc15",
-      outlineColor: "#facc15",
-      boxShadow: "0 0 1rem #facc15",
+      outlineColor: yellow,
+      boxShadow: `0 0 1rem ${yellow}`,
     },
   };
   const currentPath = usePathname();
@@ -42,22 +48,13 @@ function Button({
   const ref = useRef<HTMLButtonElement>(null!);
   const [scope, animate] = useAnimate();
 
-  const expandingCircle = async (
-    e: React.PointerEvent<HTMLButtonElement>,
-    buttonVariant: "basic" | "special",
-    out?: boolean,
-  ) => {
-    if (variant !== buttonVariant) return;
+  const [circleSize, setCircleSize] = useState<number>();
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
+  const handleBasicHover = async (e: React.PointerEvent<HTMLButtonElement>) => {
     const styles = {
-      basic: {
-        opacity: [0.3, 0],
-        scale: [0, 3],
-      },
-      special: {
-        scale: out ? [3, 0] : [0, 3],
-      },
-      path: {},
+      opacity: [0.3, 0],
+      scale: [0, 3],
     };
 
     const bounds = ref.current.getBoundingClientRect();
@@ -68,12 +65,38 @@ function Button({
 
     await animate(
       scope.current,
-      { x: [x], y: [y], ...styles[buttonVariant] },
+      { x: [x], y: [y], ...styles },
       {
         duration: 0.5,
         times: [0, 1],
       },
     );
+  };
+
+  const handleSpecialHover = async (
+    e: React.PointerEvent<HTMLButtonElement>,
+  ) => {
+    updateMousePosition(e);
+    const bounds = ref.current.getBoundingClientRect();
+
+    const x = e.clientX - bounds.x;
+    const y = e.clientY - bounds.y;
+
+    const maxDistance = Math.max(
+      Math.hypot(x, y),
+      Math.hypot(bounds.width - x, y),
+      Math.hypot(x, bounds.height - y),
+      Math.hypot(bounds.width - x, bounds.height - y),
+    );
+
+    setCircleSize(maxDistance * 2);
+  };
+
+  const updateMousePosition = (e: React.PointerEvent<HTMLButtonElement>) => {
+    const bounds = ref.current.getBoundingClientRect();
+    const x = e.clientX - bounds.x;
+    const y = e.clientY - bounds.y;
+    setMousePosition({ x, y });
   };
 
   return (
@@ -85,6 +108,12 @@ function Button({
         variant === "special" && "text-bg-primary z-10 bg-yellow-400",
         className,
       )}
+      style={
+        {
+          "--mouse-x": `${mousePosition.x}px`,
+          "--mouse-y": `${mousePosition.y}px`,
+        } as React.CSSProperties
+      }
       animate={{
         opacity: currentPath === targetPath ? 1 : undefined,
       }}
@@ -96,26 +125,56 @@ function Button({
       ref={ref}
       onPointerUp={async (e) => {
         if (disableBgEffect) return;
-        expandingCircle(e, "basic");
+        handleBasicHover(e);
       }}
       onPointerEnter={async (e) => {
         if (disableBgEffect) return;
-        expandingCircle(e, "special");
+        handleSpecialHover(e);
       }}
       onPointerLeave={async (e) => {
         if (disableBgEffect) return;
-        expandingCircle(e, "special", true);
+        updateMousePosition(e);
+        setCircleSize(undefined);
       }}
     >
       {children}
 
-      <motion.div
-        ref={scope}
-        className={cn(
-          "absolute top-0 left-0 z-[-1] aspect-square w-full rounded-full bg-white opacity-1",
-          variant === "special" && "bg-bg-primary",
-        )}
-      />
+      {variant === "basic" && (
+        <motion.div
+          ref={scope}
+          className={cn(
+            "absolute top-0 left-0 z-1 aspect-square w-full rounded-full bg-white opacity-1",
+          )}
+        />
+      )}
+      {variant === "special" && (
+        <AnimatePresence>
+          {circleSize !== undefined && (
+            <motion.div
+              ref={scope}
+              className={cn(
+                "bg-bg-primary absolute top-0 left-0 z-1 h-full w-full",
+                "overflow-hidden p-2",
+                className,
+              )}
+              style={{
+                color: yellow,
+              }}
+              initial={{
+                clipPath: "circle(0px at var(--mouse-x) var(--mouse-y))",
+              }}
+              animate={{
+                clipPath: `circle(${circleSize}px at var(--mouse-x) var(--mouse-y))`,
+              }}
+              exit={{
+                clipPath: "circle(0px at var(--mouse-x) var(--mouse-y))",
+              }}
+            >
+              {children}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      )}
     </motion.button>
   );
 }
