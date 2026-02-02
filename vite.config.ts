a37c043ec12febeff0ef635d84203e46de957d10
mingli202 <vincentmingli@gmail.com>
2026-02-01 21:28:43 -0500

    fresh packages from tanstart

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath, URL } from "url";
+
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 
 const config = defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   plugins: [
     devtools(),
     nitro(),
@@ -16,11 +23,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
-    viteReact({
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
+    viteReact(),
   ],
 });
 
