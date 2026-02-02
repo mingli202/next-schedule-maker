85b938a1c39e36c3cdfb846a8e55c5921d808c71
mingli202 <vincentmingli@gmail.com>
2026-02-02 18:05:56 -0500

    added meta data

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -9,8 +9,14 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
+import { Route as EditorRouteRouteImport } from './routes/editor/route'
 import { Route as IndexRouteImport } from './routes/index'
 
+const EditorRouteRoute = EditorRouteRouteImport.update({
+  id: '/editor',
+  path: '/editor',
+  getParentRoute: () => rootRouteImport,
+} as any)
 const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
@@ -19,28 +25,39 @@ const IndexRoute = IndexRouteImport.update({
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/editor': typeof EditorRouteRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/editor': typeof EditorRouteRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
+  '/editor': typeof EditorRouteRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/editor'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/editor'
+  id: '__root__' | '/' | '/editor'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  EditorRouteRoute: typeof EditorRouteRoute
 }
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
+    '/editor': {
+      id: '/editor'
+      path: '/editor'
+      fullPath: '/editor'
+      preLoaderRoute: typeof EditorRouteRouteImport
+      parentRoute: typeof rootRouteImport
+    }
     '/': {
       id: '/'
       path: '/'
@@ -53,6 +70,7 @@ declare module '@tanstack/react-router' {
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  EditorRouteRoute: EditorRouteRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
-import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
 import { TanStackDevtools } from "@tanstack/react-devtools";
+import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
 import appCss from "./globals.css?url";
@@ -15,7 +15,50 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "JAC Dream Schedule Builder",
+      },
+      {
+        name: "description",
+        content:
+          "John Abbott College (JAC) Dream Schedule Builder Home Page. An interactive schedule builder for John Abbott College (JAC) students. By using our advanced filtering options, you can make your dream schedule in seconds!",
+      },
+      {
+        name: "author",
+        content: "Ming Li Liu",
+      },
+      {
+        name: "creator",
+        content: "Ming Li Liu",
+      },
+      {
+        name: "generator",
+        content: "Tanstack Start",
+      },
+      {
+        name: "applicationName",
+        content: "JAC Dream Schedule Builder",
+      },
+      {
+        name: "keywords",
+        content: [
+          "Schedule Maker",
+          "Schedule Builder",
+          "Schedule Visualiser",
+          "John Abbott College",
+          "JAC",
+          "Dream Schedule Maker",
+          "Editor",
+          "Schedule Planner",
+          "Next Js",
+          "React",
+          "Javascript",
+          "JS",
+          "TailwindCss",
+        ].join(", "),
+      },
+      {
+        name: "google-site-verification",
+        content: "cSh40L1cc_UacQ_WYgMrNFYCIdZcRopjr8AWnUyDCoY",
       },
     ],
     links: [
