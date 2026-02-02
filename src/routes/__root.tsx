bcccffb944aec04c30215f6b2299d6790fb24e86
mingli202 <vincentmingli@gmail.com>
2026-02-02 18:16:57 -0500

    seo stuff free

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -66,6 +66,10 @@ export const Route = createRootRoute({
         rel: "stylesheet",
         href: appCss,
       },
+      {
+        rel: "manifest",
+        href: "/manifest.json",
+      },
     ],
   }),
 
