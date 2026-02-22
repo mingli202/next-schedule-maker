d1b57837859a685e11e43eb1c54c37ecf7fd90de
mingli202 <vincentmingli@gmail.com>
2026-02-22 14:42:05 -0500

    fix: api changes

--- src/components/LecLab.tsx
+++ src/components/LecLab.tsx
@@ -1,35 +1,32 @@
 import { Clock, User } from "lucide-react";
 import type { HTMLProps } from "react";
+import type { LecLabResponse } from "@/client";
 import cn from "@/lib/cn";
-import { capitalize, getSectionTimes } from "@/lib/util";
-import type { Section } from "@/types/generated";
+import { capitalize } from "@/lib/util";
 import TeacherStats from "./TeacherStats";
 
 type Props = {
-  section: Section;
+  leclab: LecLabResponse;
 } & HTMLProps<HTMLDivElement>;
-export default function LecLab({ section, className }: Props) {
-  return section.times.map((time, i) => (
-    <div
-      className={cn("bg-secondary mt-2 rounded-md p-2", className)}
-      key={section.id + time.title + i.toString()}
-    >
-      <h4 className="italic">{capitalize(time.type ?? "lecture")}</h4>
+export default function LecLab({ leclab, className }: Props) {
+  return (
+    <div className={cn("bg-secondary mt-2 rounded-md p-2", className)}>
+      <h4 className="italic">{capitalize(leclab.type ?? "lecture")}</h4>
 
       <div className="relative flex items-center gap-2">
         <User className="h-4 opacity-50" />
-        {time.prof}
-        <TeacherStats teacher={time.prof} />
+        {leclab.prof}
+        <TeacherStats leclab={leclab} />
       </div>
 
-      {getSectionTimes(time.time).map(([d, t], index) => {
+      {leclab.dayTimes.map((dayTime) => {
         return (
-          <p className="flex items-center gap-2" key={d + t + index.toString()}>
+          <p className="flex items-center gap-2" key={dayTime.id}>
             <Clock className="h-4 opacity-50" />
-            {d} {t}
+            {dayTime.day} {dayTime.startTimeHhmm}-{dayTime.endTimeHhmm}
           </p>
         );
       })}
     </div>
-  ));
+  );
 }
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,11 +1,3 @@
-import type { Time } from "@/types/generated";
-
-export function getSectionTimes(times: Time): string[][] {
-  return Object.entries(times).flatMap(([day, time]) =>
-    time.map((t) => [day, t]),
-  );
-}
-
 export function capitalize(s: string): string {
   return s.slice(0, 1).toUpperCase() + s.slice(1);
 }
