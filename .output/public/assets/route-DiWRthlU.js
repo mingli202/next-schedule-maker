6bc814967eebb37d9a428a482cb3dfe2771b88c6
mingli202 <vincentmingli@gmail.com>
2026-02-23 15:33:52 -0500

    removed output di

--- .output/public/assets/route-DiWRthlU.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as n}from"./main-C89q-grd.js";function c(){const o=t.c(1);let e;return o[0]===Symbol.for("react.memo_cache_sentinel")?(e=n.jsx("div",{children:'Hello "/login"!'}),o[0]=e):e=o[0],e}export{c as component};
--- .output/server/_ssr/route-Dfqqw5rJ.mjs
+++
@@ -1,7 +0,0 @@
-import { j as jsxRuntimeExports } from "../_libs/react.mjs";
-function RouteComponent() {
-  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", { children: 'Hello "/editor"!' });
-}
-export {
-  RouteComponent as component
-};
