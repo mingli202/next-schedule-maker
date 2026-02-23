8c1cc0381d3a29a6e16f387c7b1e486ae70cdccb
mingli202 <vincentmingli@gmail.com>
2026-02-23 15:05:46 -0500

    asdf

--- src/components/root/BgAnimation.tsx
+++ src/components/root/BgAnimation.tsx
@@ -30,7 +30,7 @@ export function BgAnimation() {
     <>
       <div className="from-bg-primary/75 to-primary/50 absolute top-0 left-0 -z-10 h-[200%] w-full bg-linear-to-b" />
       <div className="absolute top-0 left-0 -z-20 h-full w-full">
-        {Array(vw > 768 ? 10 : 1)
+        {Array(vw > 768 ? 10 : 5)
           .fill(0)
           .map((_, i) => (
             <MovingSchedule
