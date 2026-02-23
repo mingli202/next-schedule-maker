e47153aeca9cf752707d20946bb5cf9c4f1ea1e1
mingli202 <vincentmingli@gmail.com>
2026-02-22 22:16:27 -0500

    everything bun:test

--- src/lib/test-util.ts
+++ src/lib/test-util.ts
@@ -1,13 +1,13 @@
-import { describe, type SuiteFactory, type TestFunction, test } from "vitest";
+import { describe, test } from "bun:test";
 
-export function given(what: string, f: SuiteFactory) {
+export function given(what: string, f: () => void) {
   describe(`GIVEN ${what}`, f);
 }
 
-export function when(action: string, f: SuiteFactory) {
+export function when(action: string, f: () => void) {
   describe(`WHEN ${action}`, f);
 }
 
-export function then(behavior: string, f: TestFunction) {
+export function then(behavior: string, f: () => void) {
   test(`THEN ${behavior}`, f);
 }
