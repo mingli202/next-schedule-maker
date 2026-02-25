29f05cdc27af5139b000a45f86157da88208437b
mingli202 <vincentmingli@gmail.com>
2026-02-24 22:44:11 -0500

    user vro

--- convex/user/mutations.ts
+++ convex/user/mutations.ts
@@ -1,12 +1,17 @@
+import { Id } from "../_generated/dataModel";
 import { mutation } from "../_generated/server";
 import { getUserIdFromFirebaseId } from "./helpers";
 
-export const createUserIfNotExist = mutation({
-  handler: async (ctx) => {
+export const createUser = mutation({
+  handler: async (ctx): Promise<Id<"users">> => {
     const { user, firebaseId } = await getUserIdFromFirebaseId(ctx);
 
     if (!user) {
-      ctx.db.insert("users", { firebaseId, collectionPolicy: "on" });
+      return ctx.db.insert("users", {
+        firebaseId,
+        collectionPolicy: "on",
+      });
     }
+    return user._id;
   },
 });
