cec91486bab3d17b683b1627798f1b9143d5ba07
mingli202 <vincentmingli@gmail.com>
2026-02-28 18:13:29 -0500

    renamed vite variables

--- src/integrations/firebase.ts
+++ src/integrations/firebase.ts
@@ -6,23 +6,23 @@ const isProd = import.meta.env.PROD;
 
 const config = isProd
   ? {
-      apiKey: import.meta.env.apiKey,
-      authDomain: import.meta.env.authDomain,
-      databaseURL: import.meta.env.databaseURL,
-      projectId: import.meta.env.projectId,
-      storageBucket: import.meta.env.storageBucket,
-      messagingSenderId: import.meta.env.messagingSenderId,
-      appId: import.meta.env.appId,
-      measurementId: import.meta.env.measurementId,
+      apiKey: import.meta.env.VITE_apiKey,
+      authDomain: import.meta.env.VITE_authDomain,
+      databaseURL: import.meta.env.VITE_databaseURL,
+      projectId: import.meta.env.VITE_projectId,
+      storageBucket: import.meta.env.VITE_storageBucket,
+      messagingSenderId: import.meta.env.VITE_messagingSenderId,
+      appId: import.meta.env.VITE_appId,
+      measurementId: import.meta.env.VITE_measurementId,
     }
   : {
-      apiKey: import.meta.env.DEV_apiKey,
-      authDomain: import.meta.env.DEV_authDomain,
-      databaseURL: import.meta.env.DEV_databaseURL,
-      projectId: import.meta.env.DEV_projectId,
-      storageBucket: import.meta.env.DEV_storageBucket,
-      messagingSenderId: import.meta.env.DEV_messagingSenderId,
-      appId: import.meta.env.DEV_appId,
+      apiKey: import.meta.env.VITE_DEV_apiKey,
+      authDomain: import.meta.env.VITE_DEV_authDomain,
+      databaseURL: import.meta.env.VITE_DEV_databaseURL,
+      projectId: import.meta.env.VITE_DEV_projectId,
+      storageBucket: import.meta.env.VITE_DEV_storageBucket,
+      messagingSenderId: import.meta.env.VITE_DEV_messagingSenderId,
+      appId: import.meta.env.VITE_DEV_appId,
     };
 
 const app = initializeApp(config);
