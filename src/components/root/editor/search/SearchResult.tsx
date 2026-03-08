55606d61f3ca04ebf54fc592e99000ffe81feb14
mingli202 <vincentmingli@gmail.com>
2026-03-08 13:13:49 -0400

    feat: inifnite scroll search results

--- src/components/root/editor/search/SearchResult.tsx
+++ src/components/root/editor/search/SearchResult.tsx
@@ -1,11 +1,46 @@
 import { useSearch } from "@tanstack/react-router";
+import { Virtuoso } from "react-virtuoso";
+import type { SectionResponse } from "src/client";
+import SectionCard from "src/components/SectionCard";
 import { useSectionQuery } from "src/hooks/useSection";
+import { cn } from "src/lib/utils";
 
 export function SearchResult() {
   const search = useSearch({ from: "/editor/search" });
   const q = search.q;
 
-  const { data: result } = useSectionQuery({ q });
+  const { data: sections, isPending } = useSectionQuery({ q });
 
-  return <div className="flex-1"></div>;
+  if (isPending) {
+    return null;
+  }
+
+  return (
+    <div className="flex-1">
+      {!sections || sections.length === 0 ? (
+        <NoResult />
+      ) : (
+        <Result sections={sections} />
+      )}
+    </div>
+  );
+}
+
+function NoResult() {
+  return null;
+}
+
+type ResultProps = {
+  sections: SectionResponse[];
+};
+function Result({ sections }: ResultProps) {
+  return (
+    <Virtuoso
+      style={{ height: "100%" }}
+      data={sections}
+      itemContent={(index, section) => (
+        <SectionCard section={section} className={cn(index !== 0 && "mt-2")} />
+      )}
+    />
+  );
 }
