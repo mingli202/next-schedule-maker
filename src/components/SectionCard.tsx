7af3de888d58ec279d028b8c6c3e79c23a5d21f2
mingli202 <vincentmingli@gmail.com>
2026-03-08 15:27:46 -0400

    section card take optional footer

--- src/components/SectionCard.tsx
+++ src/components/SectionCard.tsx
@@ -1,39 +1,47 @@
-import type { HTMLProps } from "react";
+import type { HTMLProps, ReactNode } from "react";
 import type { SectionResponse } from "@/client";
 import { cn } from "@/lib/utils";
 import LecLab from "./LecLab";
 
 type SectionCardProps = {
   section: SectionResponse;
+  footer?: ReactNode;
 } & HTMLProps<HTMLDivElement>;
 
 export default function SectionCard({
   section,
+  footer,
   className,
   ...props
 }: SectionCardProps) {
   return (
     <div
-      className={cn("bg-secondary/60 flex flex-col rounded-xl p-2", className)}
+      className={cn(
+        "bg-secondary/60 flex flex-col gap-2 rounded-xl p-2",
+        className,
+      )}
       {...props}
     >
-      <h2 className="font-light">
-        {section.course}: {section.domain} {section.code}
-      </h2>
+      <div>
+        <h2 className="font-light">
+          {section.course}: {section.domain} {section.code}
+        </h2>
 
-      <h1 className="font-heading text-base font-bold md:text-xl">
-        {section.section} {section.title}
-      </h1>
+        <h1 className="font-heading text-base font-bold md:text-xl">
+          {section.section} {section.title}
+        </h1>
+      </div>
 
       {section.leclabs.map((leclab) => (
         <LecLab
           key={leclab.id}
           leclab={leclab}
-          className={cn("mt-2 rounded-md p-2")}
+          className={cn("rounded-md p-2")}
         />
       ))}
 
-      {section.more !== "" && <p className="mt-2 opacity-70">{section.more}</p>}
+      {section.more !== "" && <p className="opacity-70">{section.more}</p>}
+      {footer}
     </div>
   );
 }
