25462393ff82a4910619ba3cbf62c49a062f5dfa
mingli202 <vincentmingli@gmail.com>
2026-03-09 06:36:37 -0400

    store the fn in a ref

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,17 +1,30 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 /**
  * Custom hook that returns a debounced version of the given function that gets called after the given debounceTime
  * */
 export function useDebounce(fn: () => void, debounceTime: number) {
   const t = useRef<number>(null);
+  const fnRef = useRef(fn);
+
+  useEffect(() => {
+    fnRef.current = fn;
+  }, [fn]);
 
   const debouncedFunction = useCallback(() => {
     if (t.current) {
       window.clearTimeout(t.current);
     }
-    t.current = window.setTimeout(fn, debounceTime);
-  }, [fn, debounceTime]);
+    t.current = window.setTimeout(fnRef.current, debounceTime);
+  }, [debounceTime]);
+
+  useEffect(() => {
+    return () => {
+      if (t.current) {
+        window.clearTimeout(t.current);
+      }
+    };
+  }, []);
 
   return debouncedFunction;
 }
