fc994d3a57b4167fd6e34795af9b0c15e576d6e5
mingli202 <vincentmingli@gmail.com>
2026-03-09 20:01:23 -0400

    schedule store with persist

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,5 +1,6 @@
 import type { SectionResponse } from "src/client";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import isValidAdditionToSchedule from "./schedule/isValidAdditionToSchedule";
 
 type ScheduleState = {
@@ -12,28 +13,39 @@ type ScheduleAction = {
   remove: (sectionId: number) => boolean;
 };
 
-const useScheduleStore = create<ScheduleState & ScheduleAction>((set, get) => ({
-  sections: [],
-  set: (sections) => {
-    set({ sections });
-    return true;
-  },
-  add: (section) => {
-    if (!isValidAdditionToSchedule(section, get().sections)) {
-      return false;
-    }
+type ScheduleStore = ScheduleState & ScheduleAction;
 
-    set((state) => ({ sections: [...state.sections, section] }));
-    return true;
-  },
-  remove: (sectionId: number) => {
-    if (!get().sections.some((section) => section.id === sectionId)) {
-      return false;
-    }
+export const useScheduleStore = create<ScheduleStore>()(
+  persist(
+    (set, get) => ({
+      sections: [],
+      set: (sections) => {
+        set({ sections });
+        return true;
+      },
+      add: (section) => {
+        if (!isValidAdditionToSchedule(section, get().sections)) {
+          return false;
+        }
 
-    set((state) => ({
-      sections: state.sections.filter((section) => section.id !== sectionId),
-    }));
-    return true;
-  },
-}));
+        set((state) => ({ sections: [...state.sections, section] }));
+        return true;
+      },
+      remove: (sectionId: number) => {
+        if (!get().sections.some((section) => section.id === sectionId)) {
+          return false;
+        }
+
+        set((state) => ({
+          sections: state.sections.filter(
+            (section) => section.id !== sectionId,
+          ),
+        }));
+        return true;
+      },
+    }),
+    {
+      name: "schedule-store",
+    },
+  ),
+);
