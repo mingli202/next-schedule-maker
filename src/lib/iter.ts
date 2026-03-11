22a8ec156c80108bee3d5fbf90e86dd32187d2b2
mingli202 <vincentmingli@gmail.com>
2026-03-10 23:33:59 -0400

    all and any

--- src/lib/iter.ts
+++ src/lib/iter.ts
@@ -2,16 +2,80 @@ import { type IOption, Some } from "./option";
 
 type IterOp<T, U> = (val: IOption<T>, index: number) => IOption<U>;
 
+/**
+ * Custom implementation of a lazy iterator
+ * Every oporator will be executed once per elements,
+ * and there will only be a single pass through when consuming the iterator,
+ * making it more efficient than working with arrays
+ * */
 export class Iter<TInitial, TCurrent> {
   private constructor(
     private arr: TInitial[],
     private op: IterOp<TInitial, TCurrent>,
   ) {}
 
+  /**
+   * Creates a new Iter from a given list of array
+   * */
   public static from<T>(arr: T[]): Iter<T, T> {
     return new Iter(arr, (val) => val);
   }
 
+  /**
+   * Tests if every element of the iterator matches a predicate.
+   *
+   * all() takes a closure that returns true or false. It applies this closure to each element of the iterator, and if they all return true, then so does all(). If any of them return false, it returns false.
+   *
+   * all() is short-circuiting; in other words, it will stop processing as soon as it finds a false, given that no matter what else happens, the result will also be false.
+   *
+   * an empty iterator returns true
+   * */
+  public all(predicate: (val: TCurrent, index: number) => boolean): boolean {
+    let acc = true;
+
+    for (let i = 0; i < this.arr.length; i++) {
+      const res = this.op(new Some(this.arr[i]), i);
+      if (res.isSome()) {
+        acc &&= predicate(res.unwrap(), i);
+      }
+
+      if (!res) {
+        return false;
+      }
+    }
+
+    return acc;
+  }
+
+  /**
+   * Tests if any element of the iterator matches a predicate.
+   *
+   * any() takes a closure that returns true or false. It applies this closure to each element of the iterator, and if any of them return true, then so does any(). If they all return false, it returns false.
+   *
+   * any() is short-circuiting; in other words, it will stop processing as soon as it finds a true, given that no matter what else happens, the result will also be true.
+   *
+   * An empty iterator returns false.
+   * */
+  public any(predicate: (val: TCurrent, index: number) => boolean): boolean {
+    let acc = false;
+
+    for (let i = 0; i < this.arr.length; i++) {
+      const res = this.op(new Some(this.arr[i]), i);
+      if (res.isSome()) {
+        acc ||= predicate(res.unwrap(), i);
+      }
+
+      if (res) {
+        return true;
+      }
+    }
+
+    return acc;
+  }
+
+  /**
+   * Collects
+   * */
   public collect(): TCurrent[] {
     const initialValue: TCurrent[] = [];
     return this.fold(initialValue, (acc, val) => [...acc, val]);
