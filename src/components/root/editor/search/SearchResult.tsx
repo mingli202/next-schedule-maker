40f16e414ee8913fbd92966c1425810eeb95b446
mingli202 <vincentmingli@gmail.com>
2026-03-11 11:22:01 -0400

    fixed overflow

--- src/components/root/editor/search/SearchResult.tsx
+++ src/components/root/editor/search/SearchResult.tsx
@@ -30,67 +30,65 @@ export function SearchResult() {
     return null;
   }
 
-  return (
-    <div className="flex-1">
-      {!sections || sections.length === 0 ? (
-        <NoResult />
-      ) : (
-        <Result sections={sections} />
-      )}
-    </div>
+  return !sections || sections.length === 0 ? (
+    <NoResult />
+  ) : (
+    <Result sections={sections} />
   );
 }
 
 function NoResult() {
   return (
-    <div className="flex flex-col gap-4 overflow-x-hidden overflow-y-auto p-4">
-      <h3 className="font-heading text-center text-xl">Guidelines</h3>
-      <div className="flex flex-col gap-2">
-        <div className="flex items-center gap-2">
-          <Search className="h-4 shrink-0" /> General search
+    <div className="flex-1 overflow-x-hidden overflow-y-auto">
+      <div className="flex flex-col gap-4 p-4">
+        <h3 className="font-heading text-center text-xl">Guidelines</h3>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-2">
+            <Search className="h-4 shrink-0" /> General search
+          </div>
+          <div className="flex items-center gap-2">
+            <Filter className="h-4 shrink-0" /> Advanced filtering
+          </div>
+          <div className="flex items-center gap-2">
+            <Star className="h-4 shrink-0" /> Generate an exhaustive list of
+            candidate schedule
+          </div>
+          <div className="flex items-center gap-2">
+            <Folder className="h-4 shrink-0" /> Your saved schedules
+          </div>
+          <div className="flex items-center gap-2">
+            <Settings className="h-4 shrink-0" /> Some options
+          </div>
         </div>
-        <div className="flex items-center gap-2">
-          <Filter className="h-4 shrink-0" /> Advanced filtering
+        <div className="bg-secondary h-0.5 w-full rounded-full" />
+        <div className="">
+          Search by keywords and separate them by a comma. It will attempt to
+          search by matching various patterns. Click on the{" "}
+          <Info className="inline h-4" /> icon right to the search bar for more
+          detail. Examples:
         </div>
-        <div className="flex items-center gap-2">
-          <Star className="h-4 shrink-0" /> Generate an exhaustive list of
-          candidate schedule
-        </div>
-        <div className="flex items-center gap-2">
-          <Folder className="h-4 shrink-0" /> Your saved schedules
-        </div>
-        <div className="flex items-center gap-2">
-          <Settings className="h-4 shrink-0" /> Some options
-        </div>
+        <ul className="list-inside list-disc">
+          <li>ENGLISH, 603-200, r{">"}4.5, 10:00-16:00</li>
+          <li>HUMA, blended, steven</li>
+          <li>bio ii, s{">"}80, WF</li>
+        </ul>
+        <div className="bg-secondary h-0.5 w-full rounded-full" />
+        <p>
+          DISCLAIMER: This website is a tool meant to help students plan their
+          schedule. It is NOT an official website and you still need to make
+          your schedule via OMNIVOX. Moreover, although I try my best to make
+          the website as reliable as possible, it is still your responsibility
+          to make sure that the info is correct.
+        </p>
+        <div className="bg-secondary h-0.5 w-full rounded-full" />
+        <a
+          href="https://github.com/mingli202/next-schedule-maker"
+          className="flex items-center gap-2"
+        >
+          <Github className="h-4 shrink-0" />
+          Contribute
+        </a>
       </div>
-      <div className="bg-secondary h-0.5 w-full rounded-full" />
-      <div className="">
-        Search by keywords and separate them by a comma. It will attempt to
-        search by matching various patterns. Click on the{" "}
-        <Info className="inline h-4" /> icon right to the search bar for more
-        detail. Examples:
-      </div>
-      <ul className="list-inside list-disc">
-        <li>ENGLISH, 603-200, r{">"}4.5, 10:00-16:00</li>
-        <li>HUMA, blended, steven</li>
-        <li>bio ii, s{">"}80, WF</li>
-      </ul>
-      <div className="bg-secondary h-0.5 w-full rounded-full" />
-      <p>
-        DISCLAIMER: This website is a tool meant to help students plan their
-        schedule. It is NOT an official website and you still need to make your
-        schedule via OMNIVOX. Moreover, although I try my best to make the
-        website as reliable as possible, it is still your responsibility to make
-        sure that the info is correct.
-      </p>
-      <div className="bg-secondary h-0.5 w-full rounded-full" />
-      <a
-        href="https://github.com/mingli202/next-schedule-maker"
-        className="flex items-center gap-2"
-      >
-        <Github className="h-4 shrink-0" />
-        Contribute
-      </a>
     </div>
   );
 }
@@ -116,7 +114,7 @@ function Result({ sections }: ResultProps) {
 
   return (
     <Virtuoso
-      style={{ height: "100%" }}
+      style={{ overflowX: "hidden", width: "100%", flexGrow: 1 }}
       data={sections}
       itemContent={(index, section) => (
         <SectionCard
