d0236034693507f3a4866d5c800d4d7d61c9bd2e
mingli202 <vincentmingli@gmail.com>
2026-03-11 16:58:26 -0400

    function to fetch section store

--- src/lib/store/section.ts
+++ src/lib/store/section.ts
@@ -4,36 +4,38 @@ import type { SectionStore } from "src/types";
 
 export const allSectionsQueryOptions = queryOptions({
   queryKey: ["section-store"],
-  queryFn: async (): Promise<SectionStore> => {
-    const res = await getAllSectionsAllGet();
-    const sections = res.data ?? [];
-
-    const sectionsById = Object.fromEntries(
-      sections.map((section) => [section.id, section] as const),
-    );
-
-    const professors = new Set(
-      sections
-        .flatMap((section) => section.leclabs.map((leclab) => leclab.prof))
-        .filter((prof) => prof.trim() !== ""),
-    );
-
-    const codes = new Set(
-      sections
-        .map((section) => section.code)
-        .filter((code) => code.trim() !== ""),
-    );
-
-    return {
-      sectionsById,
-      professors,
-      codes,
-    } as const;
-  },
+  queryFn: () => fetchStore(),
   staleTime: Infinity,
 });
 
 export function useSectionStore() {
   const { data } = useSuspenseQuery(allSectionsQueryOptions);
   return data;
+}
+
+export async function fetchStore(): Promise<SectionStore> {
+  const res = await getAllSectionsAllGet();
+  const sections = res.data ?? [];
+
+  const sectionsById = Object.fromEntries(
+    sections.map((section) => [section.id, section] as const),
+  );
+
+  const professors = new Set(
+    sections
+      .flatMap((section) => section.leclabs.map((leclab) => leclab.prof))
+      .filter((prof) => prof.trim() !== ""),
+  );
+
+  const codes = new Set(
+    sections
+      .map((section) => section.code)
+      .filter((code) => code.trim() !== ""),
+  );
+
+  return {
+    sectionsById,
+    professors,
+    codes,
+  } as const;
 }
