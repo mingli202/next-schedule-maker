4a7f88b677d3c73c9380f974cbf263f9e01f35f7
mingli202 <vincentmingli@gmail.com>
2026-06-01 15:40:25 -0400

    refac: schedule no longer using grid

--- src/components/root/editor/autobuild/Schedule.tsx
+++ src/components/root/editor/autobuild/Schedule.tsx
@@ -1,5 +1,5 @@
 import { ChevronRight } from "lucide-react";
-import { Fragment, useRef } from "react";
+import { Fragment } from "react";
 import { getColorFromIndex } from "src/lib/colors";
 import { useSectionStore } from "src/lib/store/section";
 import { cn } from "src/lib/utils";
@@ -17,84 +17,87 @@ function Schedule({ schedule, index }: Props) {
   return (
     <div
       className={cn(
-        "relative box-border grid w-full grid-cols-[repeat(auto-fit,minmax(20rem,1fr))]",
-        "bg-secondary/50 shrink-0 gap-2 rounded-md p-2",
+        "relative box-border flex w-full flex-col",
+        "bg-secondary/50 gap-2 rounded-md p-2",
       )}
     >
-      <div className="grid h-40 w-full grid-cols-5 grid-rows-[repeat(20,1fr)] overflow-hidden rounded-md bg-slate-300">
-        {schedule.map(({ sectionId, colorIndex }, index) => {
-          const section = sectionsById.get(sectionId);
+      <div className="flex w-full flex-wrap gap-2">
+        <div className="box-border grid h-40 min-w-80 grow basis-1/2 grid-cols-5 grid-rows-[repeat(20,1fr)] overflow-hidden rounded-md bg-slate-300">
+          {schedule.map(({ sectionId, colorIndex }, index) => {
+            const section = sectionsById.get(sectionId);
 
-          if (!section) {
-            return null;
-          }
+            if (!section) {
+              return null;
+            }
 
-          const { textColor, bgColor } = getColorFromIndex(colorIndex);
+            const { textColor, bgColor } = getColorFromIndex(colorIndex);
 
-          return (
-            <Fragment
-              key={`${sectionId}${index}${section.code}${section.section}`}
-            >
-              {section.viewData.map((c, i) => {
-                const [day, [start, end]] = Object.entries(c)[0];
-
-                return (
-                  <div
-                    key={`${day + section.code + section.section}${i}`}
-                    style={{
-                      backgroundColor: bgColor,
-                      color: textColor,
-                      gridColumn: day,
-                      gridRowStart: start,
-                      gridRowEnd: end,
-                    }}
-                    className="flex items-center justify-center overflow-hidden text-xs"
-                  >
-                    {index + 1}
-                  </div>
-                );
-              })}
-            </Fragment>
-          );
-        })}
-      </div>
-
-      <div>
-        {schedule.map(({ sectionId, colorIndex }, i) => {
-          const section = sectionsById.get(sectionId);
-
-          if (!section) {
-            return null;
-          }
-
-          const { textColor, bgColor } = getColorFromIndex(colorIndex);
-
-          const leclab = section.leclabs.find((leclab) => leclab.prof !== "");
-
-          return (
-            <div
-              key={`${bgColor}${i.toString()}`}
-              className="flex items-center gap-2 text-sm"
-            >
-              <div
-                className="flex h-5 w-5 items-center justify-center overflow-hidden rounded-sm"
-                style={{ backgroundColor: bgColor, color: textColor }}
+            return (
+              <Fragment
+                key={`${sectionId}${index}${section.code}${section.section}`}
               >
-                {i + 1}
+                {section.viewData.map((c, i) => {
+                  const [day, [start, end]] = Object.entries(c)[0];
+
+                  return (
+                    <div
+                      key={`${day + section.code + section.section}${i}`}
+                      style={{
+                        backgroundColor: bgColor,
+                        color: textColor,
+                        gridColumn: day,
+                        gridRowStart: start,
+                        gridRowEnd: end,
+                      }}
+                      className="flex items-center justify-center overflow-hidden text-xs"
+                    >
+                      {index + 1}
+                    </div>
+                  );
+                })}
+              </Fragment>
+            );
+          })}
+        </div>
+
+        <div className="box-border grow">
+          {schedule.map(({ sectionId, colorIndex }, i) => {
+            const section = sectionsById.get(sectionId);
+
+            if (!section) {
+              return null;
+            }
+
+            const { textColor, bgColor } = getColorFromIndex(colorIndex);
+
+            const leclab = section.leclabs.find((leclab) => leclab.prof !== "");
+
+            return (
+              <div
+                key={`${bgColor}${i.toString()}`}
+                className="flex items-center gap-2 text-sm"
+              >
+                <div
+                  className="flex h-5 w-5 items-center justify-center overflow-hidden rounded-sm"
+                  style={{ backgroundColor: bgColor, color: textColor }}
+                >
+                  {i + 1}
+                </div>
+                <div>
+                  <p className="font-bold">
+                    {section.code} {section.title}
+                  </p>
+                  <p>
+                    {section.section} {leclab?.prof}
+                  </p>
+                </div>
               </div>
-              <div>
-                <p className="font-bold">
-                  {section.code} {section.title}
-                </p>
-                <p>
-                  {section.section} {leclab?.prof}
-                </p>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-      <div className="col-span-full flex items-center justify-between">
+
+      <div className="flex items-center justify-between">
         <span className="ml-1">{index + 1}</span>
         <div className="flex">
           <Button variant="basic" className="w-fit"></Button>
