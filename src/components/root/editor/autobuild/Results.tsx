51228c56f7930106e9c64634790891ba79c892c7
mingli202 <vincentmingli@gmail.com>
2026-06-03 08:15:54 -0400

    added number of schedules generated

--- src/components/root/editor/autobuild/Results.tsx
+++ src/components/root/editor/autobuild/Results.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "@tanstack/react-router";
 import { memo, useCallback, useMemo } from "react";
-import { Virtuoso } from "react-virtuoso";
+import { type Components, Virtuoso } from "react-virtuoso";
 import Button from "src/components/Button";
 import { cn } from "src/lib/utils";
 import type { SavedSection } from "src/types/schedule";
@@ -55,11 +55,19 @@ type Props = {
 
 export default memo(
   ({ generatedSchedules, onReturn, onScroll, initialScroll }: Props) => {
-    const components = useMemo(
+    const components: Components<
+      {
+        sectionId: number;
+        colorIndex: number;
+      }[]
+    > = useMemo(
       () => ({
         EmptyPlaceholder: () => <NoResult />,
+        Header: () => (
+          <div>{generatedSchedules.length} schedules generated</div>
+        ),
       }),
-      [],
+      [generatedSchedules.length],
     );
 
     return (
