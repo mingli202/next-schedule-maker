7fb382009db5d4ed2cdc9d69a0851e20aeafd924
mingli202 <vincentmingli@gmail.com>
2026-06-07 08:37:21 -0400

    using component

--- src/routes/editor/saved.tsx
+++ src/routes/editor/saved.tsx
@@ -1,9 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import SavedSchedules from "src/components/root/editor/saved/SavedShedules";
 
-export const Route = createFileRoute('/editor/saved')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/editor/saved"!</div>
-}
+export const Route = createFileRoute("/editor/saved")({
+  component: SavedSchedules,
+});
