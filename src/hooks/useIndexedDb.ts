95f3d2c897a06a883bdc8121f296c6dee50ac0f5
mingli202 <vincentmingli@gmail.com>
2026-06-06 22:42:33 -0400

    feat: useIndexedDb custom hook

---
+++ src/hooks/useIndexedDb.ts
@@ -0,0 +1,69 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { indexedDbDelete, indexedDbGet, indexedDbSet } from "src/lib/store/db";
+import { isFunction } from "src/lib/utils";
+import type { IndexedDbRecord, IndexedDbStoreName } from "src/types/indexedDb";
+
+/**
+ * Load/save data in indexedDb
+ * */
+export function useIndexedDb<T extends IndexedDbStoreName>(
+  storeName: T,
+  key: string,
+  onLoad?: (v: IndexedDbRecord<T> | null) => void,
+) {
+  const keyRef = useRef(key);
+  const storeNameRef = useRef(storeName);
+  const onLoadRef = useRef(onLoad);
+  const abortController = useRef(new AbortController());
+
+  const [value, setValue] = useState<IndexedDbRecord<T> | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    indexedDbGet(storeNameRef.current, keyRef.current).then((res) => {
+      setValue(res);
+
+      if (onLoadRef.current) {
+        onLoadRef.current(res);
+      }
+    });
+
+    return () => {
+      abortController.current.abort();
+    };
+  }, []);
+
+  const update = useCallback(
+    (
+      newValue:
+        | (IndexedDbRecord<T> | null)
+        | ((prev: IndexedDbRecord<T> | null) => IndexedDbRecord<T> | null),
+    ) => {
+      setValue((prev) => {
+        const nextValue = isFunction(newValue) ? newValue(prev) : newValue;
+
+        if (nextValue == null) {
+          indexedDbDelete(
+            storeNameRef.current,
+            keyRef.current,
+            abortController.current.signal,
+          ).catch((e) => setError(e));
+        } else {
+          indexedDbSet(
+            storeNameRef.current,
+            keyRef.current,
+            nextValue,
+            abortController.current.signal,
+          ).catch((e) => setError(e));
+        }
+
+        return nextValue;
+      });
+    },
+    [],
+  );
+
+  const remove = useCallback(() => update(null), [update]);
+
+  return { value, update, remove, error } as const;
+}
