36034be2e2910bb5b2bf7bd38d60aefe7db132a3
mingli202 <vincentmingli@gmail.com>
2026-06-07 07:46:03 -0400

    fix: saved list

--- src/components/SavedList/SavedList.tsx
+++ src/components/SavedList/SavedList.tsx
@@ -1,62 +1,45 @@
-// "use client";
-//
-// import { AnimatePresence } from "framer-motion";
-// import { useState } from "react";
-// import ScheduleCard from "./ScheduleCard";
-//
-// type Props = {
-//   savedSchedules: Record<string, Saved>;
-//   setSavedSchedules?: React.Dispatch<
-//     React.SetStateAction<Record<string, Saved> | undefined>
-//   >;
-//   allClasses: Record<string, Class>;
-//   noEdit?: boolean;
-//   select?: boolean;
-//   stateType: StateType;
-//   customSelect?: (id: string, s: Saved) => void;
-// };
-//
-// export default function SavedList({
-//   savedSchedules,
-//   setSavedSchedules,
-//   allClasses,
-//   noEdit,
-//   select,
-//   stateType,
-//   customSelect,
-// }: Props) {
-//   const [highlight, setHighlight] = useState<string>();
-//
-//   return (
-//     <div className="basis-full overflow-x-hidden overflow-y-auto max-md:text-sm">
-//       <div className="grid grid-cols-[repeat(auto-fit,minmax(7rem,1fr))] gap-1 md:grid-cols-[repeat(auto-fit,minmax(10rem,1fr))]">
-//         <AnimatePresence>
-//           {Object.entries(savedSchedules)
-//             .filter(([, s]) => s.semester === "winter2026")
-//             .map(([id, s]) => {
-//               return (
-//                 <ScheduleCard
-//                   setSavedSchedules={setSavedSchedules}
-//                   highlight={highlight}
-//                   handleHighlight={() => {
-//                     if (!select) return;
-//                     setHighlight(id);
-//                   }}
-//                   schedule={s}
-//                   key={id}
-//                   scheduleId={id}
-//                   allClasses={allClasses}
-//                   initial={{ opacity: 0, y: 5 }}
-//                   animate={{ opacity: 1, y: 0 }}
-//                   disableEdit={noEdit}
-//                   stateType={stateType}
-//                   customSelect={customSelect}
-//                 />
-//               );
-//             })}
-//           <div className="col-span-full h-0 bg-transparent" />
-//         </AnimatePresence>
-//       </div>
-//     </div>
-//   );
-// }
+import type { SavedSchedule } from "convex/types";
+import { AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import ScheduleCard from "./ScheduleCard";
+
+type Props = {
+  savedSchedules: SavedSchedule[];
+  onScheduleDelete: (scheduleId: string) => void;
+  onSheduleNameChange: (oldName: string, newName: string) => void;
+  noEdit?: boolean;
+};
+
+export default function SavedList({
+  savedSchedules,
+  onScheduleDelete,
+  onSheduleNameChange,
+  noEdit,
+}: Props) {
+  const [selectedId, setSelectedId] = useState<string>();
+
+  return (
+    <div className="basis-full overflow-x-hidden overflow-y-auto max-md:text-sm">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(7rem,1fr))] gap-1 md:grid-cols-[repeat(auto-fit,minmax(10rem,1fr))]">
+        <AnimatePresence>
+          {Object.entries(savedSchedules).map(([id, s]) => {
+            return (
+              <ScheduleCard
+                schedule={s}
+                key={id}
+                selectedId={selectedId}
+                onScheduleSelect={setSelectedId}
+                onScheduleDelete={onScheduleDelete}
+                onSheduleNameChange={onSheduleNameChange}
+                initial={{ opacity: 0, y: 5 }}
+                animate={{ opacity: 1, y: 0 }}
+                disableEdit={noEdit}
+              />
+            );
+          })}
+          <div className="col-span-full h-0 bg-transparent" />
+        </AnimatePresence>
+      </div>
+    </div>
+  );
+}
