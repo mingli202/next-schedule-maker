37962505e6cc3d83b1ebbec14782395736a56ad2
mingli202 <vincentmingli@gmail.com>
2026-06-07 07:00:05 -0400

    updared types of query

--- convex/schedules/queries.ts
+++ convex/schedules/queries.ts
@@ -1,8 +1,9 @@
 import { query } from "../_generated/server";
 import { getUserIdFromFirebaseId } from "../user/helpers";
+import { SavedSchedule } from "../types";
 
 export const getSchedules = query({
-  handler: async (ctx) => {
+  handler: async (ctx): Promise<SavedSchedule[] | undefined> => {
     const { user } = await getUserIdFromFirebaseId(ctx);
 
     if (!user) return;
@@ -12,6 +13,24 @@ export const getSchedules = query({
       .withIndex("by_userId_source", (q) => q.eq("userId", user._id))
       .collect();
 
-    return schedules;
+    return await Promise.all(
+      schedules.map(async (schedule) => {
+        const sections = await ctx.db
+          .query("sections")
+          .withIndex("by_scheduleId", (q) => q.eq("scheduleId", schedule._id))
+          .collect();
+
+        return {
+          id: schedule._id,
+          creationTime: schedule._creationTime,
+          name: schedule.name,
+          source: schedule.source,
+          sections: sections.map(({ sectionId, colorIndex }) => ({
+            sectionId,
+            colorIndex,
+          })),
+        } satisfies SavedSchedule;
+      }),
+    );
   },
 });
--- convex/types.ts
+++ convex/types.ts
@@ -6,3 +6,17 @@ export const CollectionPolicy = v.union(
   v.literal("anonymous"),
 );
 export type CollectionPolicy = typeof CollectionPolicy.type;
+
+export const SavedSchedule = v.object({
+  id: v.id("schedules"),
+  creationTime: v.number(),
+  name: v.string(),
+  source: v.string(),
+  sections: v.array(
+    v.object({
+      sectionId: v.number(),
+      colorIndex: v.number(),
+    }),
+  ),
+});
+export type SavedSchedule = typeof SavedSchedule.type;
