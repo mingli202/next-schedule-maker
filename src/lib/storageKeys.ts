473c224dded0bf29f8fd61a7ddb52a7fcb34e144
mingli202 <vincentmingli@gmail.com>
2026-06-06 22:04:31 -0400

    types

--- src/lib/storageKeys.ts
+++ src/lib/storageKeys.ts
@@ -1,8 +1,14 @@
 export const SessionStorageKey = {
-  AutobuilderOptions: "autobuilder-options",
-  AutobuilderGenerationCacheMetadata: "autobuilder-generation-cache-metadata",
+  AUTOBUILDER_OPTIONS: "autobuilder-options",
+  AUTOBUILDER_GENERATION_CACHE_METADATA:
+    "autobuilder-generation-cache-metadata",
 } as const;
 
 export const LocalStorageKey = {
-  SavedSchedules: "saved-schedules",
-};
+  SAVED_SCHEDULES: "saved-schedules",
+} as const;
+
+export const IndexedDbKey = {
+  GENERATED_SCHEDULES_CACHE_STORE: "generated-schedules-cache",
+  SAVED_SCHEDULES_STORE: "saved-schedules-store",
+} as const;
---
+++ src/types/indexedDb.ts
@@ -0,0 +1,25 @@
+import { IndexedDbKey } from "src/lib/storageKeys";
+import { z } from "zod";
+import type { RecordValues } from ".";
+import { SavedSection } from "./schedule";
+
+const IndexedDbSchemaValueBase = z.object({
+  key: z.string(),
+  updatedAt: z.number(),
+});
+
+export const IndexedDbSchema = {
+  [IndexedDbKey.GENERATED_SCHEDULES_CACHE_STORE]: z.object({
+    ...IndexedDbSchemaValueBase.shape,
+    schedules: z.array(z.array(SavedSection)),
+  }),
+  [IndexedDbKey.SAVED_SCHEDULES_STORE]: z.object({
+    ...IndexedDbSchemaValueBase.shape,
+    savedSchedules: z.array(z.array(SavedSection)),
+  }),
+} as const;
+
+export type IndexedDbStoreName = RecordValues<typeof IndexedDbKey>;
+export type IndexedDbRecord<T extends IndexedDbStoreName> = z.infer<
+  (typeof IndexedDbSchema)[T]
+>;
