480ab2143897c097db5fdc0bc69ef2993e29e613
mingli202 <vincentmingli@gmail.com>
2026-06-07 10:25:28 -0400

    save dismiss preference

--- src/components/root/editor/saved/LoginIndicator.tsx
+++ src/components/root/editor/saved/LoginIndicator.tsx
@@ -1,17 +1,32 @@
 import { useConvexAuth } from "@convex-dev/react-query";
 import { Link } from "@tanstack/react-router";
+import { X } from "lucide-react";
+import { useLocalStorage } from "src/hooks";
+import { LocalStorageKey } from "src/lib/storageKeys";
 
 export function LoginIndicator() {
   const { isLoading, isAuthenticated } = useConvexAuth();
-  console.log("isAuthenticated:", isAuthenticated);
-  console.log("isLoading:", isLoading);
 
-  if (isLoading || isAuthenticated) {
+  const [dimiss, setDismiss] = useLocalStorage(
+    false,
+    LocalStorageKey.DISMISS_LOGGEDOUT_NOTICE,
+  );
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (isAuthenticated) {
+    setDismiss(false);
+    return null;
+  }
+
+  if (dimiss) {
     return null;
   }
 
   return (
-    <div className="w-full shrink-0 gap-2 rounded-sm border border-amber-600 bg-amber-900 p-1">
+    <div className="w-full shrink-0 rounded-sm border border-amber-600 bg-amber-900 p-1">
       <div>
         You are not logged in, you won't have access to your schedules on other
         devices.{" "}
@@ -19,6 +34,14 @@ export function LoginIndicator() {
           Login
         </Link>
       </div>
+      <button
+        type="button"
+        className="flex items-center gap-1 text-left hover:cursor-pointer hover:underline"
+        onClick={() => setDismiss(true)}
+      >
+        <X className="h-4 w-4" />
+        don't show again
+      </button>
     </div>
   );
 }
