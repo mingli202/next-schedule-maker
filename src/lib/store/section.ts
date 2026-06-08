d03b2e7cf14f877319b828bf06a10ca45a20758b
mingli202 <vincentmingli@gmail.com>
2026-06-07 23:03:49 -0400

    update store to fetch from github

--- src/lib/store/section.ts
+++ src/lib/store/section.ts
@@ -1,6 +1,7 @@
 import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
-import { getAllSectionsAllGet } from "src/client";
 import type { SectionStore } from "src/types";
+import { zSectionResponse } from "src/types/client/zod.gen";
+import { z } from "zod";
 
 export const allSectionsQueryOptions = queryOptions({
   queryKey: ["section-store"],
@@ -14,8 +15,12 @@ export function useSectionStore() {
 }
 
 export async function fetchStore(): Promise<SectionStore> {
-  const res = await getAllSectionsAllGet();
-  const sections = res.data ?? [];
+  const res = await fetch(
+    "https://raw.githubusercontent.com/mingli202/scraper/refs/heads/main/data/RPHOR200_-_Schedule_of_classes_June_5/all_sections_final.json",
+  );
+
+  const json = await res.json();
+  const sections = z.array(zSectionResponse).parse(json);
 
   const sectionsById = new Map(
     sections.map((section) => [section.id, section] as const),
