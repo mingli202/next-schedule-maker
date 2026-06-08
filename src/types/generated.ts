f6c808cfb2df089148a164259448209515bb83d3
mingli202 <vincentmingli@gmail.com>
2026-06-08 11:22:08 -0400

    generated types??

---
+++ src/types/generated.ts
@@ -0,0 +1,46 @@
+import z from "zod";
+
+export const ViewData = z.array(z.record(z.string(), z.array(z.number())));
+
+export type ViewData = z.infer<typeof ViewData>;
+
+export const Section = z.object({
+	id: z.string(),
+	course: z.string(),
+	section: z.string(),
+	domain: z.string(),
+	code: z.string(),
+	title: z.string(),
+	leclabs: z.array(LecLab),
+	more: z.string(),
+	viewData: ViewData,
+});
+export type Section = z.infer<typeof Section>;
+
+export const Rating = z.object({
+	prof: z.string(),
+	score: z.number(),
+	avg: z.number(),
+	nRating: z.number(),
+	takeAgain: z.number(),
+	difficulty: z.number(),
+	status: Status,
+	pId: z.union([z.string(), z.null()]),
+});
+export type Rating = z.infer<typeof Rating>;
+
+export const LecLab = z.object({
+	title: z.string(),
+	type: z.union([LecLabType, z.null()]),
+	prof: z.string(),
+	rating: z.union([Rating, z.null()]),
+	dayTimes: z.array(DayTime),
+});
+export type LecLab = z.infer<typeof LecLab>;
+
+export const DayTime = z.object({
+	day: z.string(),
+	startTimeHhmm: z.string(),
+	endTimeHhmm: z.string(),
+});
+export type DayTime = z.infer<typeof DayTime>;
