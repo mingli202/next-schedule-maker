c4505d50ee32ec634017b8910a6a735396a7f524
mingli202 <vincentmingli@gmail.com>
2026-06-08 16:57:16 -0400

    global all sections

--- src/types/generated.ts
+++ src/types/generated.ts
@@ -59,3 +59,9 @@ export const Section = z.object({
 	viewData: ViewData,
 });
 export type Section = z.infer<typeof Section>;
+
+export const GlobalAllSections = z.object({
+	semester: z.string(),
+	sectionsById: z.record(z.string(), z.record(z.string(), Any)),
+});
+export type GlobalAllSections = z.infer<typeof GlobalAllSections>;
