c5c71ea064848c7850ce929725d4394d7f42ac6c
mingli202 <vincentmingli@gmail.com>
2026-06-15 16:50:30 -0400

    simple footer

--- src/components/root/editor/StatusFooter.tsx
+++ src/components/root/editor/StatusFooter.tsx
@@ -1,23 +1,41 @@
+import { Link } from "@tanstack/react-router";
 import { useConvexAuth } from "convex/react";
+import { getAuth, signOut } from "firebase/auth";
+import { useCallback } from "react";
 import { Button } from "src/components";
 import { ButtonVariant } from "src/components/Button";
+import { app } from "src/integrations/firebase";
 import { useSectionStore } from "src/lib/store/section";
 
 export function StatusFooter() {
   const store = useSectionStore();
   const { isLoading, isAuthenticated } = useConvexAuth();
 
+  const logOut = useCallback(async () => {
+    const auth = getAuth(app);
+    await signOut(auth);
+  }, []);
+
   return (
     <div className="bg-secondary/50 flex items-center justify-between text-xs">
       <div>
         {isLoading ? null : isAuthenticated ? (
-          <Button className="rounded-none py-1" variant={ButtonVariant.Special}>
-            Account
+          <Button
+            className="rounded-none py-1"
+            variant={ButtonVariant.Special}
+            onClick={logOut}
+          >
+            Log out
           </Button>
         ) : (
-          <Button className="rounded-none py-1" variant={ButtonVariant.Special}>
-            Log in
-          </Button>
+          <Link to="/login">
+            <Button
+              className="rounded-none py-1"
+              variant={ButtonVariant.Special}
+            >
+              Log in
+            </Button>
+          </Link>
         )}
       </div>
       <p className="flex gap-1">
