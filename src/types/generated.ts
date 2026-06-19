1078b0dd609492b978ee7de73e976f2e37396e34
mingli202 <vincentmingli@gmail.com>
2026-06-18 22:12:18 -0400

    updated generated types

--- src/types/generated.ts
+++ src/types/generated.ts
@@ -11,8 +11,8 @@ export const Status = z.enum(StatusEnum);
 export type Status = z.infer<typeof Status>;
 
 export const LecLabTypeEnum = {
-  LECTURE: "lecture",
-  LAB: "laboratory",
+	LECTURE: "lecture",
+	LAB: "laboratory",
 } as const;
 
 export const LecLabType = z.enum(LecLabTypeEnum);
@@ -20,57 +20,58 @@ export const LecLabType = z.enum(LecLabTypeEnum);
 export type LecLabType = z.infer<typeof LecLabType>;
 
 export const DayTime = z.object({
-  day: z.string(),
-  startTimeHhmm: z.string(),
-  endTimeHhmm: z.string(),
+	day: z.string(),
+	startTimeHhmm: z.string(),
+	endTimeHhmm: z.string(),
 });
 export type DayTime = z.infer<typeof DayTime>;
 
 export const Rating = z.object({
-  prof: z.string(),
-  score: z.number(),
-  avg: z.number(),
-  nRating: z.number(),
-  takeAgain: z.number(),
-  difficulty: z.number(),
-  status: Status,
-  pId: z.union([z.string(), z.null()]),
+	prof: z.string(),
+	score: z.number(),
+	avg: z.number(),
+	nRating: z.number(),
+	takeAgain: z.number(),
+	difficulty: z.number(),
+	status: Status,
+	pId: z.union([z.string(), z.null()]),
 });
 export type Rating = z.infer<typeof Rating>;
 
 export const LecLab = z.object({
-  title: z.string(),
-  type: z.union([LecLabType, z.null()]),
-  prof: z.string(),
-  rating: z.union([Rating, z.null()]),
-  dayTimes: z.array(DayTime),
+	title: z.string(),
+	type: z.union([LecLabType, z.null()]),
+	prof: z.string(),
+	rating: z.union([Rating, z.null()]),
+	dayTimes: z.array(DayTime),
 });
 export type LecLab = z.infer<typeof LecLab>;
 
 export const Section = z.object({
-  id: z.string(),
-  course: z.string(),
-  section: z.string(),
-  domain: z.string(),
-  code: z.string(),
-  title: z.string(),
-  leclabs: z.array(LecLab),
-  more: z.string(),
-  viewData: ViewData,
+	id: z.string(),
+	course: z.string(),
+	section: z.string(),
+	domain: z.string(),
+	code: z.string(),
+	title: z.string(),
+	leclabs: z.array(LecLab),
+	more: z.string(),
+	viewData: ViewData,
 });
 export type Section = z.infer<typeof Section>;
 
 export const SectionsDiff = z.object({
-  previousSectionsChanged: z.array(Section),
-  sectionsAdded: z.array(z.string()),
-  sectionsRemoved: z.array(Section),
+	previousSectionsChanged: z.array(Section),
+	sectionsAdded: z.array(z.string()),
+	sectionsRemoved: z.array(Section),
 });
 export type SectionsDiff = z.infer<typeof SectionsDiff>;
 
 export const GlobalAllSections = z.object({
-  semester: z.string(),
-  sectionsById: z.record(z.string(), Section),
-  filename: z.string(),
-  sectionsDiff: SectionsDiff,
+	semester: z.string(),
+	sectionsById: z.record(z.string(), Section),
+	filename: z.string(),
+	sectionsDiff: z.union([SectionsDiff, z.null()]),
+	comments: z.array(z.string()),
 });
 export type GlobalAllSections = z.infer<typeof GlobalAllSections>;
