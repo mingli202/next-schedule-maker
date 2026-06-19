eacc5fab0570957e26835ec4ec786ef890bebc89
mingli202 <vincentmingli@gmail.com>
2026-06-18 22:29:35 -0400

    yes

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,7 +1,7 @@
 import type { RecordValues } from "@/types";
 
 export const DataVersionCommit = {
-  Fall2026: "a11fcec7b9150eb6f5b142260cc9f317e195285d",
+  Fall2026: "main",
 } as const;
 export const LatestVersionCommit = DataVersionCommit.Fall2026;
 
