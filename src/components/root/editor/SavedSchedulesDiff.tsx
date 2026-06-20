cbcdd9ffcb1bcade0aa885ef60b741e4adc3d74a
mingli202 <vincentmingli@gmail.com>
2026-06-20 18:38:00 -0400

    feat: actual diffs

--- src/components/root/editor/SavedSchedulesDiff.tsx
+++ src/components/root/editor/SavedSchedulesDiff.tsx
@@ -1,12 +1,22 @@
-import { ComponentProps, useMemo } from "react";
+import { Clock, User } from "lucide-react";
+import { type ComponentProps, useMemo } from "react";
+import LecLabComponent from "src/components/LecLab";
+import TeacherStats from "src/components/TeacherStats";
 import { useSavedSchedule } from "src/hooks";
+import { Iter } from "src/lib/iter";
 import isValidAdditionToSchedule from "src/lib/schedule/isValidAdditionToSchedule";
 import { useSectionStore } from "src/lib/store/section";
-import { cn } from "src/lib/utils";
-import type { Section } from "src/types/generated";
+import { capitalize, cn } from "src/lib/utils";
+import type { DayTime, LecLab, Section } from "src/types/generated";
 
-const red = "bg-red-300";
-const green = "bg-green-300";
+const red = "red-300";
+const green = "green-300";
+
+const redBg = `bg-${red}`;
+const greenBg = `bg-${green}`;
+
+const redText = `text-${red}`;
+const greenText = `text-${green}`;
 
 type ModifiedSection = {
   oldSection: Section;
@@ -92,10 +102,10 @@ export function SavedSchedulesDiff({
     <div className="bg-background ring-secondary flex basis-2/5 flex-col gap-2 rounded-md p-3 ring-2">
       <div className="flex gap-2">
         <div className="flex items-center gap-1">
-          <div className={cn("h-2 w-2 rounded-full", red)} /> old
+          <div className={cn("h-2 w-2 rounded-full", redBg)} /> old
         </div>
         <div className="flex items-center gap-1">
-          <div className={cn("h-2 w-2 rounded-full", green)} /> new
+          <div className={cn("h-2 w-2 rounded-full", greenBg)} /> new
         </div>
         <div className="flex items-center gap-1">
           <OverlappingDiv className="h-2 w-2 rounded-full" /> overlap
@@ -146,28 +156,69 @@ function ScheduleDiff({
             <SectionTimes
               section={oldSection}
               key={`old-${id}-section-${oldSection.id}`}
-              className={cn("z-10", red)}
+              className={cn("z-10", redBg)}
               index={i}
             />
             {newSection && (
               <SectionTimes
                 section={newSection}
                 key={`new-${id}-section-${newSection.id}`}
-                className={cn("z-10", green)}
+                className={cn("z-10", greenBg)}
                 index={i}
               />
             )}
           </>
         ))}
       </div>
-      {modifiedSections.map(({ oldSection, newSection }, i) => (
-        <div
-          key={`modified-${id}-section-details-${oldSection.id}`}
-          className="flex w-full gap-2"
-        >
-          <div className="rounded-md p-2">{i + 1}</div>
-        </div>
-      ))}
+
+      {modifiedSections.map(({ oldSection, newSection }, i) => {
+        const El = newSection
+          ? OverlappingDiv
+          : (props: ComponentProps<"div">) => <div {...props} />;
+
+        return (
+          <div
+            key={`modified-${id}-section-details-${oldSection.id}`}
+            className="flex w-full gap-2"
+          >
+            <El className="text-background flex h-5 w-5 shrink-0 items-center justify-center overflow-hidden rounded-sm">
+              {i + 1}
+            </El>
+            <div className="flex w-full flex-col gap-1">
+              <p>
+                {oldSection.code} {oldSection.section}
+              </p>
+              {newSection && (
+                <>
+                  {oldSection.course !== newSection.course && (
+                    <Diff
+                      oldStr={oldSection.course}
+                      newStr={newSection.course}
+                    />
+                  )}
+                  {oldSection.domain !== newSection.domain && (
+                    <Diff
+                      oldStr={oldSection.domain}
+                      newStr={newSection.domain}
+                    />
+                  )}
+                  {oldSection.title !== newSection.title && (
+                    <Diff oldStr={oldSection.title} newStr={newSection.title} />
+                  )}
+                  <LeclabsDiff
+                    oldLeclabs={oldSection.leclabs}
+                    newLecLabs={newSection.leclabs}
+                    sectionId={oldSection.id}
+                  />
+                  {oldSection.more !== newSection.more && (
+                    <Diff oldStr={oldSection.more} newStr={newSection.more} />
+                  )}
+                </>
+              )}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
@@ -193,7 +244,7 @@ function SectionTimes({
           gridRowEnd: end,
         }}
         className={cn(
-          "bg-background/30 flex items-center justify-center overflow-hidden rounded-xs text-xs text-black mix-blend-multiply",
+          "bg-background/30 text-background flex items-center justify-center overflow-hidden rounded-xs text-xs mix-blend-multiply",
           className,
         )}
       >
@@ -203,26 +254,203 @@ function SectionTimes({
   });
 }
 
+function LeclabsDiff({
+  sectionId,
+  oldLeclabs,
+  newLecLabs,
+}: {
+  sectionId: string;
+  oldLeclabs: LecLab[];
+  newLecLabs: LecLab[];
+}) {
+  const minLen = Math.min(oldLeclabs.length, newLecLabs.length);
+
+  return (
+    <>
+      {Iter.from(oldLeclabs)
+        .zip(Iter.from(newLecLabs))
+        .map(([oldLeclab, newLeclab], i) => (
+          <LecLabDiff
+            key={`${sectionId}-leclabdiff-${JSON.stringify(oldLeclab.dayTimes)}-${JSON.stringify(newLeclab.dayTimes)}-${i.toString()}`}
+            oldLeclab={oldLeclab}
+            newLeclab={newLeclab}
+          />
+        ))}
+      {Iter.from(oldLeclabs)
+        .skip(minLen)
+        .map((leclab, i) => (
+          <LecLabComponent
+            key={`${sectionId}-extraoldleclab-${JSON.stringify(leclab.dayTimes)}-${i.toString()}`}
+            leclab={leclab}
+            className={cn(redBg, "text-red-900")}
+          />
+        ))}
+      {Iter.from(newLecLabs)
+        .skip(minLen)
+        .map((leclab, i) => (
+          <LecLabComponent
+            key={`${sectionId}-extranewleclab-${JSON.stringify(leclab.dayTimes)}-${i.toString()}`}
+            leclab={leclab}
+            className={cn(greenBg, "text-green-900")}
+          />
+        ))}
+    </>
+  );
+}
+
+function LecLabDiff({
+  oldLeclab,
+  newLeclab,
+}: {
+  oldLeclab: LecLab;
+  newLeclab: LecLab;
+}) {
+  const oldLeclabType = capitalize(oldLeclab.type ?? "lecture");
+  const newLeclabType = capitalize(newLeclab.type ?? "lecture");
+
+  return (
+    <div className={cn("bg-secondary rounded-md p-2")}>
+      <h4 className="italic">
+        {oldLeclabType === newLeclabType ? (
+          oldLeclabType
+        ) : (
+          <Diff oldStr={oldLeclabType} newStr={newLeclabType} />
+        )}
+      </h4>
+
+      <div className="relative flex items-center gap-2">
+        <User className="h-4 opacity-50" />
+        {oldLeclab.prof === newLeclab.prof ? (
+          <>
+            {oldLeclab.prof}
+            <TeacherStats leclab={oldLeclab} />
+          </>
+        ) : (
+          <div className="flex gap-1">
+            <div className={cn("flex gap-1", redText)}>
+              {oldLeclab.prof}
+              <TeacherStats leclab={oldLeclab} />
+            </div>
+            <div className={cn("flex gap-1", greenText)}>
+              {newLeclab.prof}
+              <TeacherStats leclab={newLeclab} />
+            </div>
+          </div>
+        )}
+      </div>
+
+      <DaytimesDiff
+        title={oldLeclab.title}
+        oldDaytimes={oldLeclab.dayTimes}
+        newDaytimes={newLeclab.dayTimes}
+      />
+    </div>
+  );
+}
+
+function DaytimesDiff({
+  title,
+  oldDaytimes,
+  newDaytimes,
+}: {
+  title: string;
+  oldDaytimes: DayTime[];
+  newDaytimes: DayTime[];
+}) {
+  const minLen = Math.min(oldDaytimes.length, newDaytimes.length);
+
+  return (
+    <>
+      {Iter.from(oldDaytimes)
+        .zip(Iter.from(newDaytimes))
+        .map(([oldDaytime, newDaytime]) => (
+          <div
+            className="flex items-center gap-2"
+            key={
+              oldDaytime.day +
+              oldDaytime.startTimeHhmm +
+              oldDaytime.endTimeHhmm +
+              newDaytime.day +
+              newDaytime.startTimeHhmm +
+              newDaytime.endTimeHhmm +
+              title
+            }
+          >
+            <Clock className="h-4 opacity-50" />
+            <Diff oldStr={oldDaytime.day} newStr={newDaytime.day} />{" "}
+            <Diff
+              oldStr={`${oldDaytime.startTimeHhmm}-${oldDaytime.endTimeHhmm}`}
+              newStr={`${newDaytime.startTimeHhmm}-${newDaytime.endTimeHhmm}`}
+            />
+          </div>
+        ))}
+
+      {Iter.from(oldDaytimes)
+        .skip(minLen)
+        .map((dayTime) => (
+          <div
+            className={cn("flex items-center gap-2", redText)}
+            key={
+              dayTime.day + dayTime.startTimeHhmm + dayTime.endTimeHhmm + title
+            }
+          >
+            <Clock className="h-4 opacity-50" />
+            {dayTime.day} {dayTime.startTimeHhmm}-{dayTime.endTimeHhmm}
+          </div>
+        ))}
+
+      {Iter.from(newDaytimes)
+        .skip(minLen)
+        .map((dayTime) => (
+          <div
+            className={cn("flex items-center gap-2", greenText)}
+            key={
+              dayTime.day + dayTime.startTimeHhmm + dayTime.endTimeHhmm + title
+            }
+          >
+            <Clock className="h-4 opacity-50" />
+            {dayTime.day} {dayTime.startTimeHhmm}-{dayTime.endTimeHhmm}
+          </div>
+        ))}
+    </>
+  );
+}
+
 function OverlappingDiv({
   children,
   className,
   ...props
 }: ComponentProps<"div">) {
   return (
-    <div {...props} className={cn("bg-view-bg relative", className)}>
+    <div
+      {...props}
+      className={cn("bg-view-bg relative z-0 overflow-hidden", className)}
+    >
       <div
         className={cn(
-          "absolute top-0 left-0 h-full w-full mix-blend-multiply",
-          red,
+          "absolute top-0 left-0 -z-1 h-full w-full mix-blend-multiply",
+          redBg,
         )}
       />
       <div
         className={cn(
-          "absolute top-0 left-0 h-full w-full mix-blend-multiply",
-          green,
+          "absolute top-0 left-0 -z-1 h-full w-full mix-blend-multiply",
+          greenBg,
         )}
       />
       {children}
+    </div>
+  );
+}
+
+function Diff({ oldStr, newStr }: { oldStr: string; newStr: string }) {
+  return oldStr === newStr ? (
+    oldStr
+  ) : (
+    <div className="flex gap-1">
+      <p className={redText}>{oldStr}</p>
+      {" -> "}
+      <p className={greenText}>{newStr}</p>
     </div>
   );
 }
