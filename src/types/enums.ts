5564ed57ab2964dbd9d4da46c539194b767c6f4f
mingli202 <vincentmingli@gmail.com>
2026-06-21 14:44:05 -0400

    pin version

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,7 +1,7 @@
 import type { RecordValues } from "@/types";
 
 export const DataVersionCommit = {
-  Fall2026: "main",
+  Fall2026: "812acb74cd61edd4ec58a437b7abc5f7e60aff73",
 } as const;
 export const LatestVersionCommit = DataVersionCommit.Fall2026;
 
