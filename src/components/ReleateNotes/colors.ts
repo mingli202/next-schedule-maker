1d90c8830b3d0ed9f46447b6f13d6e52ce22aadf
mingli202 <vincentmingli@gmail.com>
2026-06-20 21:14:38 -0400

    ` -> "

--- src/components/ReleateNotes/colors.ts
+++ src/components/ReleateNotes/colors.ts
@@ -1,4 +1,4 @@
-export const redBg = `bg-red-300`;
-export const greenBg = `bg-green-300`;
-export const redText = `text-red-300`;
-export const greenText = `text-green-300`;
+export const redBg = "bg-red-300";
+export const greenBg = "bg-green-300";
+export const redText = "text-red-300";
+export const greenText = "text-green-300";
